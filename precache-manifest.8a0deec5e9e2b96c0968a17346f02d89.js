d75dfba189806a58146183626b5bd5fcbb9926e1
Edgar Montano <em2532@nyu.edu>
2019-07-06 14:27:35 -0400

    Updates

---
+++ precache-manifest.8a0deec5e9e2b96c0968a17346f02d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e668ef710693ac6baa5106d1ae6ee2e",
+    "url": "/usab/index.html"
+  },
+  {
+    "revision": "7f707601f67cf9d8a8d7",
+    "url": "/usab/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0dc60d85004ff77c6ea5",
+    "url": "/usab/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7f707601f67cf9d8a8d7",
+    "url": "/usab/static/js/2.8e81696d.chunk.js"
+  },
+  {
+    "revision": "0dc60d85004ff77c6ea5",
+    "url": "/usab/static/js/main.a3950203.chunk.js"
+  },
+  {
+    "revision": "5cb75e2b88268f5afb02",
+    "url": "/usab/static/js/runtime~main.004c5c0d.js"
+  }
+]);
